7e4dfca07d2169b17c0097aac2e023960c27c7b8
unknown <hv4347172@gmail.com>
2026-08-26 19:09:29 +0530

    Enable TLS for TiDB connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,13 @@ require("dotenv").config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: true
+  }
 });
 
 module.exports = pool;
