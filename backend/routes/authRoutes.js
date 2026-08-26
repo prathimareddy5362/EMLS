d400ba64d60e899ef035d80b03f957f9b8fae189
unknown <hv4347172@gmail.com>
2026-08-26 18:49:01 +0530

    Fix controller path case sensitivity

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
+
 const router = express.Router();
 
-const authController = require("../controllers/authController");
+const authController = require("../Controllers/authController");
 
 // Register
 router.post("/register", authController.register);
